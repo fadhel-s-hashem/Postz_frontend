0c96b6caddf117ff0e40bfb0b8247bbf26fc91f3
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-30 11:03:12 +0300

    postz detail appear

--- src/pages/PostzDetail.jsx
+++ src/pages/PostzDetail.jsx
@@ -2,14 +2,46 @@ import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router'
 import { useParams } from 'react-router'
 
+import * as postzServices from '../services/postzServices'
+
+
 const PostzDetail = () => {
     const navigate = useNavigate()
     const { postId } = useParams()
+
+    const [postz, setPostz] = useState(null)
+
+    useEffect(() => {
+        const fetchPostz = async () => {
+            const postzData = await postzServices.show(postId)
+            setPostz(postzData)
+        }
+        fetchPostz()
+    }, [postId])
+
+
+
+    if (!postz) return(
+    <main>
+        <div class="spinner">
+        <div class="spinner1"></div>
+        </div>
+    </main>
+) 
 
 
     return(
         <section>
             <h1>Postz Detail</h1>
+
+            <article>
+                <h2 className='PostTitle'>{postz.title}</h2>
+                <p className="postz-author">Posted by {postz.author?.username || 'Unknown user'}</p>
+                <p className="postzText">{postz.text}</p>
+                <p><span className='postDate'>
+                    {new Date(postz.createdAt).toLocaleDateString()}
+                </span></p>
+            </article>
             
 
         </section>
